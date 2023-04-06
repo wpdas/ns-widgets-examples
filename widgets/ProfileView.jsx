3087c16e777e341c1d5695140b93eb665c250759
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-06 12:01:34 -0300

    first commit with two initial widgets

---
+++ widgets/ProfileView.jsx
@@ -0,0 +1,21 @@
+const IPFS_NEAR_SOCIAL_THUMBNAIL_URL =
+  "https://i.near.social/thumbnail/https://ipfs.near.social/ipfs/";
+
+const accountId = context.accountId || "wendersonpires.near";
+const profile = socialGetr(`${accountId}/profile`);
+const profileImage = `${IPFS_NEAR_SOCIAL_THUMBNAIL_URL}${profile.image.ipfs_cid}`;
+
+return (
+  <div>
+    <img src={profileImage} alt="profile avatar" />
+    <p>from Profile view</p>
+    {/* Load UserNameAccountView widget */}
+    <Widget
+      src={"wendersonpires.near/widget/NSLVWidget"}
+      props={{
+        src: "wendersonpires.near/widget/UserNameAccountView",
+        srcProps: { name: "Wendz", accountId },
+      }}
+    />
+  </div>
+);
---
+++ widgets/UserNameAccountView.jsx
@@ -0,0 +1,9 @@
+const userName = props.name;
+const userAccoundId = props.accountId;
+
+return (
+  <div>
+    <p>test 22</p>
+    <span>{userName}</span> <span>(@{userAccoundId})</span>
+  </div>
+);
